1cf20c6fa7eb0a36d14f2fb0e42d3e2e76067195
rlalasrl <rlaalsrl0810@naver.com>
2020-08-13 15:44:34 +0900

    app.jsx +insert method

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/ToDoTemplate/TodoTemplate';
 import TodoInsert from './components/TodoInsert/TodoInsert';
 import TodoList from './components/TodoList/TodoList';
@@ -22,9 +22,22 @@ const App = () => {
     }
   ]);
 
+  //고유값으로 사용될 Id & ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  }, [todos]);
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert}/>
       <TodoList todos={todos}/>
     </TodoTemplate>
   );
